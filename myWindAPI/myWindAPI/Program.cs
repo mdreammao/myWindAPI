02ee63b8d78b426f7f9a2c39c480325cb6f11d3c
heng mao <184772181@qq.com>
2016-03-03 14:11:50 +0800

    初步的学习研究

--- myWindAPI/myWindAPI/Program.cs
+++ myWindAPI/myWindAPI/Program.cs
@@ -23,9 +23,20 @@ namespace myWindAPI
             //WindData wd = w.wset("IndexConstituent", "date=20141215;windcode=000300.SH");
             //OutputWindData(wd, "wset");
 
-            WindData wd = w.wsd("600000.SH,600004.SH", "open", "2014-10-16", "2014-12-16", "");
-            OutputWindData(wd, "wsd");
-
+            //WindData wd = w.wsd("600000.SH,600004.SH", "open", "2014-10-16", "2014-12-16", "");
+            //OutputWindData(wd, "wsd");
+            WindData d = w.tdays("20150101", "20201213", "");
+            object t=d.getDataByFunc("tdays", true);
+            WindData optionInformation = w.wset("OptionChain", "date=20150209;us_code=510050.SH;option_var=;month=全部;call_put=全部");
+            object[] tt = optionInformation.data as object[];
+            Console.WriteLine(optionInformation.data.GetType());
+            Console.WriteLine(tt.GetType());
+            foreach (var item in tt)
+            {
+                Console.WriteLine(item.ToString());
+                Console.WriteLine(item.GetType());
+            }
+           
             w.stop();
         }
 
